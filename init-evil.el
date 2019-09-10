9f7f4fa3e2937483668789ccb4ae99f4c2333abb
lhongyan <1922684631@qq.com>
2019-09-10 22:51:56 +0800

    use package manage evil

--- init-evil.el
+++ init-evil.el
@@ -9,25 +9,23 @@
 ;; Evil requires the goto-chg.el package which provides the functions goto-last-change and goto-last-change-reverse
 (require 'goto-chg)
 
-;; activate evil
-(require 'evil)
-(evil-mode 1)
-
-;; remove all keybindings from insert-state keymap
-(setcdr evil-insert-state-map nil)
-
-;; use ESC or C-[ escape insert-mode to normal-mode
-(define-key evil-insert-state-map [escape] 'evil-normal-state)
-
-;; remove default evil-toggle-key C-z。 default，Emacs use C-z hang up itself
-(setq evil-toggle-key "")
-
-;; Use j/k to move one visual line insted of gj/gk
-(define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
-(define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
-(define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
-(define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
-
+(use-package evil
+    :defer t
+    :ensure t
+    :init  
+    (evil-mode)
+    ;; remove all keybindings from insert-state keymap
+    (setcdr evil-insert-state-map nil) 
+    ;; remove all keybindings from insert-state keymap
+    (define-key evil-insert-state-map [escape] 'evil-normal-state)
+    ;; Use j/k to move one visual line insted of gj/gk
+    (define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+    (define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+    (define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+    (define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+    ;; remove default evil-toggle-key C-z。 default，Emacs use C-z hang up itself
+    (setq evil-toggle-key "")
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
