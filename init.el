a6d1dcc7280179590c9895ee85986a75a9795fb4
lhongyan <1922684631@qq.com>
2019-09-15 22:27:28 +0800

    hack emacs

--- init.el
+++ init.el
@@ -5,7 +5,11 @@
 (when (>= emacs-major-version 24)
     (require 'package)
     (package-initialize)
-    (setq package-archives '(("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+    (setq package-archives '(
+        ("melpa" . "http://elpa.emacs-china.org/melpa/")
+        ("gnu"   . "http://elpa.emacs-china.org/gnu/")
+    )
+))
 
 (require 'cl)
 
