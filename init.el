36537e5ed7f6d5d9ecf957a2245301d7c5625548
lhongyan <1922684631@qq.com>
2019-09-22 23:35:30 +0800

    hack emacs

--- init.el
+++ init.el
@@ -231,6 +231,26 @@
             "./agenda/waiting-for.org"
         )
     )
+    (defun org-insert-src-block (src-code-type)
+        (interactive
+            (let ((src-code-types
+            '("emacs-lisp" "python" "C" "sh" "java" "js" "clojure" "C++" "css"
+                "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond" "mscgen"
+                "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk" "ditaa"
+                "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
+                "scheme" "sqlite")))
+            (list (ido-completing-read "Source code type: " src-code-types)))
+        )
+        (progn
+            (newline-and-indent)
+            (insert (format "#+BEGIN_SRC %s\n" src-code-type))
+            (newline-and-indent)
+            (insert "#+END_SRC\n")
+            (previous-line 2)
+            (org-edit-src-code)
+        )
+    )
+    (setq org-src-fontify-natively t)
     :bind
     ("\C-c l" . org-store-link)
     ("\C-c a" . org-agenda)
