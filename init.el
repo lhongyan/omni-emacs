e6127419ae5ff162359ec000d43919064c0e6bc3
lhongyan <1922684631@qq.com>
2020-04-29 00:56:38 +0800

    update module

--- init.el
+++ init.el
@@ -85,22 +85,6 @@
 (setq gc-cons-threshold most-positive-fixnum)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; 公用，但不必单独成文件的配置
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; 编码
-;(setq-default pathname-coding-system 'euc-cn)
-(prefer-coding-system 'utf-8)
-(setenv "LANG" "zh_CN.UTF-8")
-(setenv "LC_ALL" "zh_CN.UTF-8")
-(setenv "LC_CTYPE" "zh_CN.UTF-8")
-
-(set-default-coding-systems 'utf-8) 
-(set-terminal-coding-system 'utf-8) 
-(set-keyboard-coding-system 'utf-8) 
-(setq default-buffer-file-coding-system 'utf-8)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; load custom
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
---
+++ modules/init-encode.el
@@ -0,0 +1,14 @@
+
+;; 编码
+;(setq-default pathname-coding-system 'euc-cn)
+(prefer-coding-system 'utf-8)
+(setenv "LANG" "zh_CN.UTF-8")
+(setenv "LC_ALL" "zh_CN.UTF-8")
+(setenv "LC_CTYPE" "zh_CN.UTF-8")
+
+(set-default-coding-systems 'utf-8) 
+(set-terminal-coding-system 'utf-8) 
+(set-keyboard-coding-system 'utf-8) 
+(setq default-buffer-file-coding-system 'utf-8)
+
+(provide 'init-encode)
