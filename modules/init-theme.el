eaf52d51171a3f3f2eb5bddc6a18d9d38be06f6c
10908 <hongyan.li@hand-china.com>
2020-04-29 21:45:16 +0800

    update

--- modules/init-theme.el
+++ modules/init-theme.el
@@ -1,12 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; 主题
+;;;; theme
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'color-theme-sanityinc-tomorrow)
-(custom-set-variables
-	'(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
-	'(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
-)
+(color-theme-sanityinc-tomorrow--define-theme night)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
--- private/README.md
+++ private/README.md
@@ -1,1 +1,6 @@
-add your config to this path
+add your config to this path.
+
+for example:
+
+- private-function.el
+- private-gtd.el
