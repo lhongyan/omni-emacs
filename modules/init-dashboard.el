7f145f79424ba1430dee8e6d04c3e102620a2cf3
10908 <hongyan.li@hand-china.com>
2020-05-01 17:05:54 +0800

    update dashboard

--- modules/init-dashboard.el
+++ modules/init-dashboard.el
@@ -2,7 +2,19 @@
 ;;;; dashboard
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package dashboard)
+(use-package dashboard
+    :ensure t
+    :init
+    (setq dashboard-banner-logo-title "Welcome to Omni Emacs")
+    (setq dashboard-startup-banner 2)
+    (setq dashboard-center-content t)
+    (setq dashboard-show-shortcuts nil)
+    (setq dashboard-items '(
+        (recents  . 20)
+        (bookmarks . 10)
+    ))
+    :config
+    (dashboard-setup-startup-hook))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
